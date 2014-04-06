e02b2d28e8b8fb8a3fc63fab1ac328d5c9acd696
Tom Minor <tornronim@gmail.com>
2014-04-06 05:23:56 +0100

    Documented the class a little

--- PaintGUI.py
+++ PaintGUI.py
@@ -1,5 +1,6 @@
 import maya.cmds as cmds
 
+#ToFix : Tweak scriptjob to be killed in different scenarios (onScene etc)
 class MeshPaintGUI:
 	def __init__(self):
 		self.curvePaths = []
@@ -31,12 +32,23 @@ class MeshPaintGUI:
 		cmds.setToolTo("CreateNurbsCircleCtx")
 
 	def onNewObject(self):
+		#Reset the user's tool to it's previous context
 		cmds.setToolTo(self.currentContext)
+		#Remove the job waiting for new scene objects
+		cmds.scriptJob(kill=self.newObjectJob)
+
 		#Grab the newest created item
 		newObject = cmds.ls()[-1]
-		#Only process valid objects (curves)
+
+		# Only process valid objects (curves) -
+		# Invalid types shouldn't be a problem since the class paint commands will
+		# start the job immediately before the user makes an object, but another script
+		# could create objects while the user is painting so this check will
+		# prevent non-curve objects from being added
 		if(cmds.objectType(newObject) == 'nurbsCurve'):
 			self.objectList.append(newObject)
+		else:
+			print "Attempted to add invalid object type to mesh of type %s" % cmds.objectType(newObject)
 
 	def debugPrintLists(self):
 		print "Curves",
